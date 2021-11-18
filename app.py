58b2cc4bcc5d32358c0c37f38aa42833d09de0dd
Austin Reilly <areilly@alphaworks.tech>
2021-11-17 16:54:03 -0800

    added paths for led high and low

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 import RPi.GPIO as gpio
+import time
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
 gpio.setup(18, gpio.OUT)
@@ -12,11 +13,22 @@ gpio.setup(18, gpio.OUT)
 @app.route("/")
 def index():
 	gpio.output(18,gpio.HIGH)
+	time.sleep(1)
+	gpio.output(18,gpio.LOW)
 	return render_template('index.html')
 
 @app.route("/escape/<var>")
 def passVariable(var):
 	return f'you passed: {escape(var)}'
+
+@app.route("/led/<state>")
+def actuate(state):
+	if state == 'on':
+		gpio.output(18,gpio.HIGH)
+	if state == 'off':
+		gpio.output(18,gpio.LOW)
+	return render_template('index.html')
+
 @app.route("/hello")
 def hello():
 	return "<h1>Hello from my rasberry pi!</h1><p>Let's Gooo!</P>"
