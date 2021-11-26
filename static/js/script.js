a52dfb3ec681921979f7d30967b733ccbebd21c9
Austin Reilly <areilly@alphaworks.tech>
2021-11-26 10:22:23 -0800

    added mobile username change xP

--- static/js/script.js
+++ static/js/script.js
@@ -4,4 +4,7 @@ function ledActuate(state){
 
 fetch('/db')
 .then(res=>res.json())
-.then(data=>document.getElementById('username').innerHTML = data.username[0]);
+.then(data=>{
+	document.getElementById('username').innerHTML = data.username[0];
+	document.getElementById('usernameMobile').innerHTML = data.username[0];
+});
