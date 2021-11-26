42560de6fa0513a527ff5246873b1f606664864e
Austin Reilly <areilly@alphaworks.tech>
2021-11-26 10:23:00 -0800

    added addUser function checkout the nifty nested fetch tho

--- static/js/userList.js
+++ static/js/userList.js
@@ -9,3 +9,31 @@ fetch('/db')
 		userList.appendChild(element);
 	}
 });
+
+function addUser(e){
+	e.preventDefault();
+	const username = document.getElementById('username-box').value;
+	const password = document.getElementById('password-box').value;
+
+	let data = {'username':username,'password':password};
+
+	console.log(username, password);
+
+	fetch('/users/newuser', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	})
+	.then(()=>{
+		fetch('/db')
+		.then(res=>res.json())
+		.then(data=>{
+			const element = document.createElement('li');
+			element.innerText = data.username[data.username.length - 1];
+			userList.appendChild(element);
+		});
+	});
+
+}
